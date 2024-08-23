01f23c5a0c8fba0a423ac7f699a5b405b0db23d7
Arjun Sharda <sharda.aj17@gmail.com>
2024-08-23 11:40:46 -0500

    first commit 🎉

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.countrypy import Country
+
+# Example usage
+country = Country("United States of America")
+print(f"Name: {country.name}")
+print(f"Official Name: {country.official_name}")
+print(f"ISO Code: {country.iso_code}")
+print(f"Phone Code: {country.phone_code}")
+print(f"Population: {country.population}")
+print(f"Languages: {country.languages}")
+print(f"Timezone: {country.timezone}")
+print(f"Currency: {country.currency}")
+print(f"Capital: {country.capital}")
+print(f"Flag: {country.flag}")
+print(f"TLD: {country.tld}")
