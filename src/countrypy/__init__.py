fd77b33892772079196c7af200bfd1a2e45e73cc
Arjun Sharda <sharda.aj17@gmail.com>
2024-08-24 15:10:37 -0500

    split files
    Signed-off-by: Arjun Sharda <sharda.aj17@gmail.com>

--- src/countrypy/__init__.py
+++ src/countrypy/__init__.py
@@ -1,5 +1,6 @@
 import requests
 from countrypy.country_data import *
+from countrypy.search import *
 
 API_URL = "https://restcountries.com/v3.1/all?fields=name,flags,cca2,capital,idd,tld,population,languages,timezones,currencies,region,subregion,independent,area,landlocked,demonyms,unMember,maps,fifa,startOfWeek"
 
